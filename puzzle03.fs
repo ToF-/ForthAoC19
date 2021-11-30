8439ea4a884fd382fe4ace4d73c0cb08a00f4287
ToF- <cthibaut@octo.com>
2021-11-30 12:33:24 +0100

    puzzle03 @intersects! stores intersections with steps

--- puzzle03.fs
+++ puzzle03.fs
@@ -1,4 +1,8 @@
 
+: WITHIN+ ( a,b,c -- b<=a<=c )
+    2DUP > IF SWAP THEN
+    1+ WITHIN ;
+
 16 CONSTANT WORD-BITS
 1 WORD-BITS LSHIFT 1- CONSTANT WORD-MASK
 1 WORD-BITS 1- LSHIFT CONSTANT SIGN-BIT
@@ -27,6 +31,7 @@ WORD-MASK -1 XOR CONSTANT NEGATIVE-CELL-MASK
     0 0 0 ;
 
 : END-WIRES ( row,col,steps -- )
+    0 0 0 0 WIRE>CELL ,
     DROP 2DROP ;
 
 : WIRE, ( row,col,steps -- row,col,steps )
@@ -59,8 +64,74 @@ WORD-MASK -1 XOR CONSTANT NEGATIVE-CELL-MASK
 : ORTHOGONAL? ( r0,c0,rn,cn,r0',c0,rn',cn' )
     VERTICAL? >R VERTICAL? R> <> ;
 
-: INTERSECT! ( steps,addr )
+: INTERSECT! ( steps,addr -- )
     SWAP >R
     DUP @ CELL>WIRE DROP R> WIRE>CELL SWAP ! ;
+
+: VERTICAL-INTERSECT? ( py,x,qy,x,y,rx,y,sx -- y,x,T|F )
+    OVER >R             \                         { y } 
+    4 PICK >R           \                         { y,x }  
+    NIP                 \ py,x,qy,x,y,rx,sx       { y,x }
+    2>R 2>R NIP         \ py,qy                   { y,x,rx,sx,x,y }
+    2R> SWAP 2R>        \ py,qy,y,x,rx,sx         { y,x }
+    WITHIN+ >R          \ py,qy,y                 { y,x,rx<=x<=sx }
+    -ROT WITHIN+        \ py<=y<=qy               { y,x,rx<=x<=sx }
+    R> AND 2R> ROT      \ y,x,py<=y<=qy&rx<=x<=sx
+    DUP 0= IF -ROT 2DROP THEN ;
+
+: 4SWAP ( a,b,c,d,e,f,g,h -- e,f,g,h,a,b,c,d )
+    2>R 2SWAP       \ a,b,e,f,c,d  { g,h }
+    2ROT 2R>        \ e,f,c,d,a,b,g,h
+    2SWAP 2ROT ;    \ e,f,g,h,a,b,c,d 
+
+: HORIZONTAL-INTERSECT? ( y,px,y,qx,ry,x,sy,x -- y,x,T|F )
+    4SWAP VERTICAL-INTERSECT? ;
+
+: DISTANCES ( y,x,py,px,qy,qx -- dy,dx )
+    2DROP ROT - ABS 
+    -ROT - ABS SWAP ;
+
+: END-WIRE? ( row,col,steps,dist -- f )
+    + + + 0= ;
+
+: 8DUP ( a,b,c,d,e,f,g,h -- a,b,c,d,e,f,g,h,a,b,c,d,e,f,g,h )
+    8 0 DO 7 PICK LOOP ;
+
+: 8DROP 
+    8 0 DO DROP LOOP ;
+
+: @WIRE>LINE ( addr -- py,px,qy,qx )
+    DUP @ CELL>WIRE 2DROP  \ addr,qy,qx
+    ROT CELL -             \ qy,qx,addr-1
+    @ CELL>WIRE 2DROP      \ qy,qx,py,px
+    2SWAP ;                \ py,px,qy,qx
+
+: @INTERSECT? ( addrA,addrB -- y,x,T|F )
+    @WIRE>LINE                         \ addrA,ry,rx,sy,sx
+    2>R 2>R @WIRE>LINE                 \ py,px,qy,qx
+    2R> 2R>                            \ py,px,qy,qx,ry,rx,sy,sx
+    8DUP VERTICAL-INTERSECT? IF        \ py,px,qy,qx,ry,rx,sy,sx,y,x
+        2>R 8DROP 2R> TRUE \ y,x,T
+    ELSE HORIZONTAL-INTERSECT? IF      \ y,x 
+            TRUE
+        ELSE                           \ py,px,qy,qx,ry,rx,sy,sx
+            FALSE
+    THEN THEN ;
     
+: @INTERSECT! ( dy,dx,addr -- )
+    >R + R>                      \ dist,addr
+    DUP CELL - @ CELL>WIRE       \ dist,addr,py,px,steps,i
+    DROP -ROT 2DROP              \ dist,addr,steps
+    ROT + SWAP                   \ steps',addr
+    INTERSECT! ;
     
+: @INTERSECTS! ( y,x,addrA,addrB )
+    2SWAP 2>R SWAP        \ addrB,addrA  { y,x }
+    DUP @WIRE>LINE       \ addrB,addrA,py,px,qy,qx
+    2R@ 2ROT 2ROT        \ addrB,addrA,y,x,py,px,qy,qx
+    DISTANCES            \ addrB,addrA,dy,dx
+    ROT @INTERSECT!      \ addrB
+    DUP @WIRE>LINE       \ addrB,ry,rx,sy,sx
+    2R> 2ROT 2ROT        \ addrB,x,y,ry,rx,sy,sx
+    DISTANCES            \ addrB,dy,dx
+    ROT @INTERSECT!     ;
--- puzzle03tests.fs
+++ puzzle03tests.fs
@@ -2,50 +2,97 @@
 INCLUDE ffl/tst.fs
 INCLUDE puzzle03.fs
 
-T{ ." WIRE>CELL converts row col steps and intersection distance to a cell" CR
+." WIRE>CELL converts row col steps and intersection distance to a cell" CR
     42 -17 4807 13 WIRE>CELL
-   ." CELL>WIRE converts a cell to row,col,steps and intersection distance" CR
+." CELL>WIRE converts a cell to row,col,steps and intersection distance" CR
    CELL>WIRE
    13 ?S 4807 ?S -17 ?S 42 ?S
-}T
 
-T{ ." WIRES creates a new list of wires with a first point at 0,0" CR
-    WIRES CIRCUIT-A
-    CIRCUIT-A @ CELL>WIRE -1 ?S 0 ?S 0 ?S 0 ?S
+
+." WIRES creates a new list of wires with a first point at 0,0" CR
+    WIRES CIRCUIT-0
+    CIRCUIT-0 @ CELL>WIRE -1 ?S 0 ?S 0 ?S 0 ?S
+." END-WIRES compiles a end of circcuit mark
     END-WIRES
-}T
+    CIRCUIT-0 CELL+ @ CELL>WIRE 0 ?S 0 ?S 0 ?S 0 ?S
 
-T{ ." WIRE- words, compile a wire to the right, with current steps, no intersection" CR
-    WIRES CIRCUIT-B
+." WIRE- words, compile a wire to the right, with current steps, no intersection" CR
+    WIRES CIRCUIT-1
     42 WIRE-R,
     17 WIRE-U,
     10 WIRE-L,
     37 WIRE-D,
     END-WIRES
-    CIRCUIT-B 1 CELLS + @ CELL>WIRE -1 ?S 42 ?S 42 ?S 0 ?S
-    CIRCUIT-B 2 CELLS + @ CELL>WIRE -1 ?S 59 ?S 42 ?S 17 ?S
-    CIRCUIT-B 3 CELLS + @ CELL>WIRE -1 ?S 69 ?S 32 ?S 17 ?S
-    CIRCUIT-B 4 CELLS + @ CELL>WIRE -1 ?S 106 ?S 32 ?S -20 ?S
-}T
+    CIRCUIT-1 1 CELLS + @ CELL>WIRE -1 ?S 42 ?S 42 ?S 0 ?S
+    CIRCUIT-1 2 CELLS + @ CELL>WIRE -1 ?S 59 ?S 42 ?S 17 ?S
+    CIRCUIT-1 3 CELLS + @ CELL>WIRE -1 ?S 69 ?S 32 ?S 17 ?S
+    CIRCUIT-1 4 CELLS + @ CELL>WIRE -1 ?S 106 ?S 32 ?S -20 ?S
 
-T{ ." VERTICAL? says if a line is vertical or not" CR
+." VERTICAL? says if a line is vertical or not" CR
     0 0 100 0 VERTICAL? TRUE ?S
     10 23 10 47 VERTICAL? FALSE ?S
-}T
 
-T{ ." ORTHOGONAL? says if two lines are orthogonal" CR
+." ORTHOGONAL? says if two lines are orthogonal" CR
     0 0 100 0  0 0 0 50 ORTHOGONAL? TRUE ?S
     0 0 0 100  0 0 0 50 ORTHOGONAL? FALSE ?S
-}T
 
-T{ ." INTERSECT! stores an intersection at a given step of a wire" CR
-    WIRES CIRCUIT-C
+." WITHIN+ tells if a value is within a range" CR
+    40 0 100 WITHIN+ TRUE ?S
+    100 0 100 WITHIN+ TRUE ?S
+    101 0 100 WITHIN+ FALSE ?S
+    -1 0 100 WITHIN+ FALSE ?S
+    -5 0 -100 WITHIN+ TRUE ?S
+    0 0 -100 WITHIN+ TRUE ?S
+    -100 0 -100 WITHIN+ TRUE ?S
+    -101 0 -100 WITHIN+ FALSE ?S
+    1 0 -100 WITHIN+ FALSE ?S
+
+." INTERSECT! stores an intersection at a given step of a wire" CR
+    WIRES CIRCUIT-2
     8 WIRE-R, 5 WIRE-U, 5 WIRE-L, 3 WIRE-D,
     END-WIRES
-    23 CIRCUIT-C 1 CELLS + INTERSECT! 
-    CIRCUIT-C 1 CELLS + @ CELL>WIRE 23 ?S 8 ?S 8 ?S 0 ?S
-}T
-    
+    23 CIRCUIT-2 1 CELLS + INTERSECT! 
+    CIRCUIT-2 1 CELLS + @ CELL>WIRE 23 ?S 8 ?S 8 ?S 0 ?S
+
+." VERTICAL-INTERSECT? tells is a vertical intersects a horizontal and at which point" CR
+    10 15 30 15 20 5 20 50 VERTICAL-INTERSECT? TRUE ?S SWAP 20 ?S 15 ?S
+    10 15 30 15  9 5  9 50 VERTICAL-INTERSECT? FALSE ?S
+    10 15 30 15 31 5 31 50 VERTICAL-INTERSECT? FALSE ?S
+    10 15 30 15 20 0 20 14 VERTICAL-INTERSECT? FALSE ?S
+    10 15 30 15 20 16 20 24 VERTICAL-INTERSECT? FALSE ?S
+." HORIZONTAL-INTERSECT? tells is a horizontak intersects a vertical and at which point" CR
+    20 5 20 50 10 15 30 15 HORIZONTAL-INTERSECT? TRUE ?S SWAP 20 ?S 15 ?S 
+
+." DISTANCES tells the distances (row and col) from the start of a line to a point of that line" CR
+    10 20 0 20 40 20 DISTANCES SWAP 10 ?S 0 ?S 
+    15 20 15 10 15 30 DISTANCES SWAP 0 ?S 10 ?S 
+    -12 10 10 10 -15 10 DISTANCES SWAP 22 ?S 0 ?S
+    -12 10 -15 10 -5 10 DISTANCES SWAP 3 ?S 0 ?S
+    34 10 34 4 34 40 DISTANCES SWAP 0 ?S 6 ?S
+    34 10 34 40 34 4 DISTANCES SWAP 0 ?S 30 ?S
+
+    WIRES CIRCUIT-A
+    \ R8,U5,L5,D3,
+    8 WIRE-R, 5 WIRE-U, 5 WIRE-L, 3 WIRE-D,
+    END-WIRES
+    WIRES CIRCUIT-B
+    \ U7,R6,D4,L4,
+    7 WIRE-U, 6 WIRE-R, 4 WIRE-D, 4 WIRE-L,
+    END-WIRES
+
+." @INTERSECT? tells if two wires intersect and at what point" CR
+    CIRCUIT-A 3 CELLS + CIRCUIT-B 3 CELLS + @INTERSECT? TRUE ?S SWAP 5 ?S 6 ?S
+    CIRCUIT-A 2 CELLS + CIRCUIT-B 3 CELLS + @INTERSECT? FALSE ?S 
+    CIRCUIT-A 3 CELLS + CIRCUIT-B 2 CELLS + @INTERSECT? FALSE ?S 
+    CIRCUIT-A 4 CELLS + CIRCUIT-B 4 CELLS + @INTERSECT? TRUE ?S SWAP 3 ?S 3 ?S
+
+." INTERSECT! stores an intersection step value at wires which intersect" CR
+    5 6 CIRCUIT-A 3 CELLS + CIRCUIT-B 3 CELLS + @INTERSECTS!
+    CIRCUIT-A 3 CELLS + @ CELL>WIRE 15 ?S DROP 2DROP 
+    CIRCUIT-B 3 CELLS + @ CELL>WIRE 15 ?S DROP 2DROP 
+    3 3 CIRCUIT-A 4 CELLS + CIRCUIT-B 4 CELLS + @INTERSECTS!
+    CIRCUIT-A 4 CELLS + @ CELL>WIRE 20 ?S DROP 2DROP 
+    CIRCUIT-B 4 CELLS + @ CELL>WIRE 20 ?S DROP 2DROP 
 
 BYE
 
